265b3937a84a8254e07dd1d3e453d13919823f6c
prosperityai <prosperpaul@ymail.com>
2019-04-01 14:22:35 +0300

    ipop

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 from scripts import tabledef
 from scripts import forms
 from scripts import helpers
@@ -7,6 +6,8 @@ from flask import Flask, redirect, url_for, render_template, request, session
 import json
 import sys
 import os
+from sklearn.externals import joblib
+model = joblib.load('model.pkl')
 
 
 app = Flask(__name__)
@@ -58,6 +59,30 @@ def signup():
             return json.dumps({'status': 'User/Pass required'})
         return render_template('login.html', form=form)
     return redirect(url_for('login'))
+
+@app.route('/prediction', methods=['GET', 'POST'])
+def prediction():
+    #col = ['age', 'mealsperday', 'readingbook', 'checkingbook', 'parent_teacher']
+    #data= np.array([meals_data, age, reading, checkbook, gender, parent_teacher])
+    if request.method == 'POST':
+        gender = request.form['gender']
+        age = request.form['age']
+        meals = request.form['meals']
+        book = request.form['book']
+        check_book = request.form['check_book']
+        parent_teacher = request.form['parent_teacher']
+        new_age = age.split('-')
+        year_born = int(new_age[0])
+        age = 2019-year_born
+        gender = int(gender);meals=int(meals);book=int(book)
+        check_book = int(check_book);parent_teacher=int(parent_teacher)
+
+        data = [[meals, age, book, check_book, gender, parent_teacher]] 
+        predicted = model.predict(data)
+        predicted = predicted[0]
+        print(predicted)
+        print(gender, age , meals, book, check_book, parent_teacher)
+        return render_template("pred.html", prediction = predicted)
 
 
 # -------- Settings ---------------------------------------------------------- #
